e73a5837e00d5e6f79537ce44b503dc1671c1041
SOHEE JEONG <page.0895059@gmail.com>
2023-09-15 22:49:52 +0900

    :sparkles: Feat: 인덱스 페이지 구성

--- config/urls.py
+++ config/urls.py
@@ -20,8 +20,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from config.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', index),
 ]
 
 urlpatterns += static(
