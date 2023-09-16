ef01602e9698a4de515070e718a1e6932d789a7c
SOHEE JEONG <page.0895059@gmail.com>
2023-09-17 00:53:25 +0900

    :sparkles: Feat: 피드 페이지 기능 구현
    Post 목록 출력하기
    - 작성자 정보 표시
    - Post의 이미지, 내용, 댓글 목록, 작성일자 등 출력
    - 메인 링크 및 로그아웃 버튼 추가

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, redirect
 
+from posts.models import Post
+
 def feeds(request) :
     # 로그인한 사용자가 아닐 경우 (AnonymousUser인 경우)
     if not request.user.is_authenticated :
         # /users/login/으로 이동
         return redirect('/users/login/')
+    
+    # 모든 글 목록을 템플릿으로 전달
+    posts = Post.objects.all()
+    context = {
+        'posts' : posts,
+    }
 
-    return render(request, 'posts/feeds.html')
+    return render(request, 'posts/feeds.html', context)
