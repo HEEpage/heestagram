11a5ef40e3ac7ebad602063932d4867198699baa
SOHEE JEONG <page.0895059@gmail.com>
2023-09-16 12:00:37 +0900

    :sparkles: Feat: users 앱 생성 및 등록
    - AbstractUser 모델을 이용한 CustomUser 정의

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Add
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Add
+AUTH_USER_MODEL = 'users.User'
