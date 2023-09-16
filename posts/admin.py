199a4e50eb09dc93543fcc35d028290c1ca57a13
SOHEE JEONG <page.0895059@gmail.com>
2023-09-16 23:43:45 +0900

    :sparkles: Feat: 피드 페이지를 위한 Model 구성 및 admin 구현
    - Post, PostImage, Comment 모델링

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+import admin_thumbnails
 
-# Register your models here.
+from posts.models import Post, PostImage, Comment
+
+
+class CommentInline(admin.TabularInline) :
+    model = Comment
+    extra = 1
+
+@admin_thumbnails.thumbnail('photo')
+class PostImageInline(admin.TabularInline) :
+    model = PostImage
+    extra = 1
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin) :
+    list_display = ['id', 'content',]
+    # ForeignKey로 연결된 Comment 객체 확인하기 위해 admin의 Inline 기능 사용
+    inlines = [CommentInline, PostImageInline,] 
+
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin) :
+    list_display = ['id', 'post', 'photo',]
+
+
+@admin.register(Comment)
+class CommentImageAdmin(admin.ModelAdmin) :
+    list_display = ['id', 'post', 'content',]
