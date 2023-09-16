26f8482fdcd12cd0ee3dc917d149f4b3b3c819bc
SOHEE JEONG <page.0895059@gmail.com>
2023-09-16 13:03:06 +0900

    :sparkles: Feat: 로그인 여부에 따른 페이지 접속 제한 추가

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def feeds(request) :
+    # 로그인한 사용자가 아닐 경우 (AnonymousUser인 경우)
+    if not request.user.is_authenticated :
+        # /users/login/으로 이동
+        return redirect('/users/login/')
+
     return render(request, 'posts/feeds.html')
