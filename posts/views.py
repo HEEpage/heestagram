08f81165296206894e5881ec3513b65cef7b70df
SOHEE JEONG <page.0895059@gmail.com>
2023-09-17 17:55:41 +0900

    :sparkles: Feat: 댓글 작성 기능 추가
    - 댓글 생성 및 작성 후 해당 Post로 위치 이동하도록 작성

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+from django.http import HttpResponseRedirect
 
 from posts.models import Post
+from posts.forms import CommentForm
 
 def feeds(request) :
     # 로그인한 사용자가 아닐 경우 (AnonymousUser인 경우)
@@ -10,8 +13,29 @@ def feeds(request) :
     
     # 모든 글 목록을 템플릿으로 전달
     posts = Post.objects.all()
+    comment_form = CommentForm()
+
     context = {
         'posts' : posts,
+        'comment_form' : comment_form,
     }
 
     return render(request, 'posts/feeds.html', context)
+
+
+# 댓글 작성을 처리할 View. POST 요청만 허용한다.
+@require_POST
+def comment_add(request) :
+    # request.POST로 전달된 데이터를 사용해 CommentForm 인스턴스를 생성
+    form = CommentForm(data=request.POST)
+
+    if form.is_valid() :
+        # commit=False 옵션으로 메모리상에 Comment 객체 생성
+        comment = form.save(commit=False)
+        # Comment 생성에 필요한 사용자 정보를 request에서 가져와 할당
+        comment.user = request.user
+        # DB에 Comment 객체 저장
+        comment.save()
+
+        # 생성 완료 후 피드 페이지로 이동
+        return HttpResponseRedirect(f'/posts/feeds/#post-{comment.post.id}')
