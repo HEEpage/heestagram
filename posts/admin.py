0da4b6cc3013c5752c9ded262f77f47457a2abef
SOHEE JEONG <page.0895059@gmail.com>
2023-09-21 15:58:45 +0900

    :sparkles: Feat: 좋아요 기능을 위한 필드 추가 및 Model 관계 설정

--- posts/admin.py
+++ posts/admin.py
@@ -15,12 +15,21 @@ class PostImageInline(admin.TabularInline) :
     model = PostImage
     extra = 1
 
+class LikeUserInline(admin.TabularInline) :
+    model = Post.like_users.through
+    verbose_name = '좋아요 한 User'
+    verbose_name_plural = f'{verbose_name} 목록'
+    extra = 1
+
+    def has_change_permission(self, request, obj=None) :
+        return False
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin) :
     list_display = ['id', 'content',]
     # ForeignKey로 연결된 Comment 객체 확인하기 위해 admin의 Inline 기능 사용
-    inlines = [CommentInline, PostImageInline,] 
+    inlines = [CommentInline, PostImageInline, LikeUserInline,] 
 
     # Post 변경 화면에서 ManyToMayField를 Checkbox로 출력
     formfield_overrides = {
